628d27dd30184b05d29f432dbf32d6bf05a0b3f8
cui_xiaorui <cui_xiaorui@126.com>
2019-06-18 16:12:40 +0800

    add error test for test travis

--- test/add.test.js
+++ test/add.test.js
@@ -8,4 +8,8 @@ describe("addFn", () => {
   test("1 add 1 should equal 2", () => {
     expect(addFn(1, 1)).not.toBe(3);
   });
+
+  test("1 add 1 should equal 4", () => {
+    expect(addFn(1, 1)).toBe(3);
+  });
 });
