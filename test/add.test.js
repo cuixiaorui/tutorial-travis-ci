a0ddeb2293e174c6a50c3471fdd3102a079d66aa
vipkid <vipkid@vipkiddeMacBook-Pro.local>
2019-06-18 15:40:15 +0800

    add test

--- test/add.test.js
+++ test/add.test.js
@@ -2,6 +2,10 @@ const addFn = require("../src/add.js");
 
 describe("addFn", () => {
   test("1 add 1 should equal 2", () => {
-    expect(addFn(1, 1));
+    expect(addFn(1, 1)).toBe(2);
+  });
+
+  test("1 add 1 should equal 2", () => {
+    expect(addFn(1, 1)).not.toBe(3);
   });
 });
