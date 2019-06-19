846bbd60062c62c588ed116c4db313443952656a
cui_xiaorui <cui_xiaorui@126.com>
2019-06-19 15:12:26 +0800

    delete error test

--- test/add.test.js
+++ test/add.test.js
@@ -4,12 +4,4 @@ describe("addFn", () => {
   test("1 add 1 should equal 2", () => {
     expect(addFn(1, 1)).toBe(2);
   });
-
-  test("1 add 1 should equal 2", () => {
-    expect(addFn(1, 1)).not.toBe(3);
-  });
-
-  test("1 add 1 should equal 4", () => {
-    expect(addFn(1, 1)).toBe(3);
-  });
 });
